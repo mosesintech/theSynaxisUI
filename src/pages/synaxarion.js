b216af46b69728e44dd8c8c5c8cf0010ec204e99
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-03-15 17:59:38 -0500

    add func to synaxarion page

---
+++ src/pages/synaxarion.js
@@ -0,0 +1,13 @@
+import Edges from '../components/layout/Edges'
+
+export default function Synaxarion() {
+  return (
+    <>
+    <Edges size="lg">
+      <div className="container mx-auto max-w-7xl px-4 py-6">
+        <h1 className="font-headingStyled">The Synaxis Lives of the Saints</h1>
+      </div>
+    </Edges>
+    </>    
+  )
+}
